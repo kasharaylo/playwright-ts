0f130291ea670b8d74710bade099d85a4d84304b
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-06-02 10:20:40 +0300

    Radio Buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -23,4 +23,18 @@ test.describe('Form Layouts page', () => {
         // Locator Assertions
         await expect(usingTheGridEmailInput).toHaveValue('email2@email.com')
     })
+
+    test('radio buttons', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+        
+        //await usingTheGridForm.getByLabel('Option 1').check({force: true}) // Check the radio button
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true}) // Check the radio button by role
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        await expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+    })
 })
