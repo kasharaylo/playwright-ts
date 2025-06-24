01f77a77827d6efab4f0773a6cbad04d109e5d98
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-06-24 12:42:07 +0300

    Locators in PageObject

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,35 +1,45 @@
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 
 export class NavigationPage {
     readonly page: Page // The page object for navigation
+    readonly formLayoutsMenuItem: Locator // Locator for the Form Layouts menu item
+    readonly datepickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
 
     constructor(page: Page) { // Initialize the page object
         this.page = page // Assign the page to the class property
+        this.formLayoutsMenuItem = page.getByText('Form Layouts') // Locator for Form Layouts menu item
+        this.datepickerMenuItem = page.getByText('Datepicker')
+        this.smartTableMenuItem = page.getByText('Smart Table')
+        this.toastrMenuItem = page.getByText('Toastr')
+        this.tooltipMenuItem = page.getByText('Tooltip')
     }
 
     async formLayoutsPage() { // Method to navigate to the Form Layouts page
         await this.selectGroupMenuItem('Forms') // Ensure the Forms group is expanded
-        await this.page.getByText('Form Layouts').click()
+        await this.formLayoutsMenuItem.click()
     }
 
     async datepickerPage() { // Method to navigate to the Datepicker page
         await this.selectGroupMenuItem('Forms') // Ensure the Datepicker group is expanded
-        await this.page.getByText('Datepicker').click()
+        await this.datepickerMenuItem.click()
     }
 
     async smartTablePage() { // Method to navigate to the Smart Table page
         await this.selectGroupMenuItem('Tables & Data')
-        await this.page.getByText('Smart Table').click()
+        await this.smartTableMenuItem.click()
     }
 
     async toastrPage() { // Method to navigate to the Toastr page
         await this.selectGroupMenuItem('Modal & Overlays') // Ensure the Modal & Overlays group is expanded
-        await this.page.getByText('Toastr').click()
+        await this.toastrMenuItem.click()
     }
 
     async tooltipPage() { // Method to navigate to the Tooltip page
         await this.selectGroupMenuItem('Modal & Overlays') // Ensure the Modal & Overlays group is expanded
-        await this.page.getByText('Tooltip').click()
+        await this.toastrMenuItem.click()
     }
 
     private async selectGroupMenuItem(groupItemTitle: string) {
