a1359075dec55b1c6c1d1c7d422fd6d683633724
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-06-24 11:43:29 +0300

    Parametrized Methods

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,32 @@
+import { Locator, Page } from "@playwright/test"
+
+export class FormLayoutsPage {
+    private readonly page: Page // The page object for Form Layouts
+
+    constructor(page: Page) { // Initialize the page object
+        this.page = page // Assign the page to the class property
+    }
+    async submitUsingTheGridFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string) {
+        const usingTheGridForm = this.page.locator('nb-card', {hasText: "Using the Grid"})
+        await usingTheGridForm.getByRole('textbox', {name: "Email"}).fill(email) // Fill the email field
+        await usingTheGridForm.getByRole('textbox', {name: "Password"}).fill(password) // Fill the password
+        await usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true}) // Check the first radio option
+        await usingTheGridForm.getByRole('button').click() // Click the Sign in button
+    }
+
+    /**
+     * This method will out the Inline form with user details
+     * @param name - should be first or last name
+     * @param email - valid email for the test user
+     * @param rememberMe - true or false if user session to be safed
+     */
+
+    async submitInlineFormWithEmailAndCheckbox(name: string, email: string, rememberMe: boolean) {
+        const inLineForm = this.page.locator('nb-card', {hasText: "Inline form"})
+        await inLineForm.getByRole('textbox', {name: "Jane Doe"}).fill(name) // Fill the email field
+        await inLineForm.getByRole('textbox', {name: "Email"}).fill(email) // Fill the password
+        if(rememberMe)
+            await inLineForm.getByRole('checkbox').check({force: true})
+        await inLineForm.getByRole('button').click()
+    }
+}
