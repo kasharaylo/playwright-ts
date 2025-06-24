d1c7c264aaf9cf7b96f15a025499abcd771e762b
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-06-24 11:43:29 +0300

    Navigations in PageObject

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -8,7 +8,35 @@ export class NavigationPage {
     }
 
     async formLayoutsPage() { // Method to navigate to the Form Layouts page
-        await this.page.getByText('Forms').click()
+        await this.selectGroupMenuItem('Forms') // Ensure the Forms group is expanded
         await this.page.getByText('Form Layouts').click()
+    }
+
+    async datepickerPage() { // Method to navigate to the Datepicker page
+        await this.selectGroupMenuItem('Forms') // Ensure the Datepicker group is expanded
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async smartTablePage() { // Method to navigate to the Smart Table page
+        await this.selectGroupMenuItem('Tables & Data')
+        await this.page.getByText('Smart Table').click()
+    }
+
+    async toastrPage() { // Method to navigate to the Toastr page
+        await this.selectGroupMenuItem('Modal & Overlays') // Ensure the Modal & Overlays group is expanded
+        await this.page.getByText('Toastr').click()
+    }
+
+    async tooltipPage() { // Method to navigate to the Tooltip page
+        await this.selectGroupMenuItem('Modal & Overlays') // Ensure the Modal & Overlays group is expanded
+        await this.page.getByText('Tooltip').click()
+    }
+
+    private async selectGroupMenuItem(groupItemTitle: string) {
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+        if (expandedState == 'false') {
+            await groupMenuItem.click() // Click to expand the group menu if it is not already expanded
+        }
     }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -8,4 +8,8 @@ test.beforeEach(async({page}, testInfo) => {
 test('Navigate to the page', async({page}) => {
     const navigateTo = new NavigationPage(page) // Create an instance of NavigationPage with the current page
     await navigateTo.formLayoutsPage() // Call the method to navigate to the Form Layouts page
+    await navigateTo.datepickerPage() // Call the method to navigate to the Datepicker page
+    await navigateTo.smartTablePage() // Call the method to navigate to the Smart Table page
+    await navigateTo.toastrPage() // Call the method to navigate to the Toastr page
+    await navigateTo.tooltipPage() // Call the method to navigate to the Tooltip page
 })
