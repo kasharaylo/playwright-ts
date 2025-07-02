c20e4928f9d51a2da1f2984d170bead6853ee4e6
Constantine Kasharaylo <kasharaylo@gmail.com>
2025-07-02 12:02:04 +0300

    Date Picker Page Object

---
+++ page-objects/datePickerPage.ts
@@ -0,0 +1,46 @@
+import { expect, Page } from "@playwright/test"
+
+export class DatePickerPage {
+    private readonly page: Page
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async selectCommonDatePickerDateFromToday(numberOfDaysFromToday: number) {
+        const calendarInputField = this.page.getByPlaceholder('Form Picker') // Locate the datepicker input field
+        await calendarInputField.click() // Open the datepicker
+        const dateAssert = await this.selectDateInTheCalendar(numberOfDaysFromToday) // Select the date in the calendar
+
+        await expect(calendarInputField).toHaveValue(dateAssert) // Assert that the input field has the expected date value
+    }
+
+    async selectDatePickerWithRange(startDay: number, endDay: number) {
+        const calendarInputField = this.page.getByPlaceholder('Range Picker') // Locate the range datepicker input field
+        await calendarInputField.click() // Open the datepicker
+        const dateAssertStart = await this.selectDateInTheCalendar(startDay) // Select the start date in the calendar
+        const dateAssertEnd = await this.selectDateInTheCalendar(endDay) // Select the end date in the calendar
+        const expectedRange = `${dateAssertStart} - ${dateAssertEnd}` // Combine the start and end dates for the expected range
+        await expect(calendarInputField).toHaveValue(expectedRange) // Assert that the input field has the expected date range value
+    }
+
+    private async selectDateInTheCalendar(numberOfDaysFromToday: number) {
+        let date = new Date() // Get the current date
+        date.setDate(date.getDate() + numberOfDaysFromToday) // Set the date to tomorrow
+        const expctDate = date.getDate().toString() // Get the expected date as a string
+        const exactMonthShort = date.toLocaleDateString('En-Us', {month: 'short'}) // Get the short month name
+        const exactMonthLong = date.toLocaleDateString('En-Us', {month: 'long'}) // Get the long month name
+        const exactYear = date.getFullYear() // Get the year
+        const dateAssert = `${exactMonthShort} ${expctDate}, ${exactYear}` // Combine the short month, date, and year for the expected value
+        
+        let calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent() // Get the current month and year from the calendar
+        const exactMonthAndYear = ` ${exactMonthLong} ${exactYear} ` // Combine the long month and year
+        while(!calendarMonthAndYear.includes(exactMonthAndYear)) { // Loop until the calendar shows the expected month and year
+            await this.page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click() 
+            calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
+        }
+        
+        await this.page.locator('.day-cell.ng-star-inserted').getByText(expctDate, {exact:true}).click() // Click on the day cell in the datepicker
+        return dateAssert // Return the expected date string
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {expect, test} from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage' // Import the NavigationPage class
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage' // Import the FormLayoutsPage class
+import { DatePickerPage } from '../page-objects/datePickerPage' // Import the DatePickerPage class
 
 test.beforeEach(async({page}, testInfo) => {
     await page.goto('http://localhost:4200/')
@@ -18,8 +19,14 @@ test('Navigate to the page', async({page}) => {
 test('parametrized methods', async({page}) => {
     const navigateTo = new NavigationPage(page) // Create an instance of NavigationPage with the current page
     const onFormLayoutsPage = new FormLayoutsPage(page) // Create an instance of FormLayoutsPage with the current page
+    const onDatePickerPage = new DatePickerPage(page) // Create an instance of DatePickerPage with the current page
 
     await navigateTo.formLayoutsPage() // Navigate to the Form Layouts page
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', "123456", 'Option 2') // Submit the form with credentials and select an option
     await onFormLayoutsPage.submitInlineFormWithEmailAndCheckbox('John Smith', 'test@test.com', false)
+
+    await navigateTo.datepickerPage() // Navigate to the Datepicker page
+    await onDatePickerPage.selectCommonDatePickerDateFromToday(10) // Select a date from the datepicker 5 days from today
+
+    await onDatePickerPage.selectDatePickerWithRange(5, 10) // Select a date range from the datepicker, from 5 to 10 days from today
 })
